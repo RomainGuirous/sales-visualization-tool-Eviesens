f05f5d2493087fb31aec7877b84cbba73266d358
RomainGuirous <romaing_59@hotmail.fr>
2023-12-14 15:39:57 +0100

    commande comment

--- read_commande.py
+++ read_commande.py
@@ -22,11 +22,12 @@ def select_commmande(df) :
     df_commande = df_commande[df_commande['Reduction'].notna()]
     df_commande = df_commande[df_commande["Date d'achat"].notna()]  ######/!\/!\ PROBLEME DATE D'ACHAT  NAN A REPARER PLUS TARD /!\ /!\ #####
     df_commande = df_commande[['Structure','Transaction','Moyen de paiement','Nom',"Date d'achat" ]] #on selectionne les colonnes utiles pour nous par leur nom dans le DF
-    df_commande = df_commande.rename(columns={'Structure': 'type_structure_nom', 'Transaction': 'type_transaction_nom', 'Moyen de paiement':'moyen_paiement_nom', "Nom":'client_id',"Date d'achat":'commande_date_achat'}) #on change nom col pour correspondre au nom dans les tables type_structure, etc
+    df_commande = df_commande.rename(columns={'Structure': 'type_structure_nom', 'Transaction': 'type_transaction_nom', 'Moyen de paiement':'moyen_paiement_nom', "Nom":'client_id',"Date d'achat":'commande_date_achat'})
+    #on change nom col pour correspondre au nom dans les tables type_structure, type_transaction, moyen_paiement et commande (pour commande_date_achat et client_id)
     return df_commande
 
 
-
+# envoie un dataframe dans la base de donnee
 def df_to_database(df, table_name, connection) :
     df.to_sql(table_name, con=connection, index=False, if_exists='append')
 
@@ -49,45 +50,44 @@ def drop_existing_name(dico, df) :
             res=res.drop(index=i)
     return res
 
+#transforme les dates format excel au format SQL
 def excel_to_sql_date(date):
-    patt1=re.compile(r"/")
-    date=re.sub(patt1,"-",date) #on transforme les "/" en "-"
-    patt2=re.compile(r"(\d\d)-(\d\d)-(\d{4})")
-    date=re.sub(patt2,r"\3-\2-\1",date) #on inverse les jours et les mois
+    date=re.sub(r"/","-",date) #on transforme les "/" en "-"
+    date=re.sub(r"(\d\d)-(\d\d)-(\d{4})",r"\3-\2-\1",date) #on inverse les jours et les mois
     return date
 
 
 #Main
 conn= create_engine('mysql+mysqlconnector://root:root@localhost:3306/eviesens')
 
-filepaths=os.listdir("./donnees/fiches_mensuelles/")
+filepaths=os.listdir("./donnees/fiches_mensuelles/") #récupère liste des noms des fichiers dans le dossier "fiches_mensuelles"
 
 for i in range(len(filepaths)) :
-    filepaths[i]="./donnees/fiches_mensuelles/"+filepaths[i]
+    filepaths[i]="./donnees/fiches_mensuelles/"+filepaths[i] #on récupère liste des filepath de chaque fiche mensuelle
 
 for filepath in filepaths :
     df=pd.read_csv(filepath)
-    df_commande=select_commmande(df)
+    df_commande=select_commmande(df) #on récupère un dataframe par mois avec les colonnes et les lignes qui nous intéressent
     
-    #dictionnaires des vendeurs / types activites avant l'insertion
+    #dictionnaires des structures / transactions / moyens de paiement avant l'insertion
     before_dico_type_structure_db=database_to_dict("type_structure",conn)
     before_dico_type_transaction_db=database_to_dict("type_transaction",conn)
     before_dico_moyen_paiement_db=database_to_dict("moyen_paiement",conn)
 
 
-    # recupere la liste des activites et envoie les nouvelles activites en bdd
+    # recupere la liste des structures et envoie les nouvelles structures en bdd
     df_type_structure=df_commande['type_structure_nom'].drop_duplicates() #on supprime doublons -> Nan
     df_type_structure=df_type_structure.dropna() #on supprime Nan
     df_type_structure=drop_existing_name(before_dico_type_structure_db, df_type_structure) #supprime les noms deja existants en bdd
     df_to_database(df_type_structure,"type_structure",conn)
 
-    # recupere la liste des vendeurs et envoie les nouveaux vendeurs en bdd
+    # recupere la liste des transactions et envoie les nouvelles transactions en bdd
     df_type_transaction=df_commande['type_transaction_nom'].drop_duplicates() #on supprime doublons -> Nan
     df_type_transaction=df_type_transaction.dropna() #on supprime Nan
     df_type_transaction=drop_existing_name(before_dico_type_transaction_db, df_type_transaction) #supprime les noms deja existants en bdd
     df_to_database(df_type_transaction,"type_transaction",conn)
 
-    # recupere la liste des vendeurs et envoie les nouveaux vendeurs en bdd
+    # recupere la liste des moyens de paiement et envoie les nouveaux moyens de paiement en bdd
     df_moyen_paiement=df_commande['moyen_paiement_nom'].drop_duplicates() #on supprime doublons -> Nan
     df_moyen_paiement=df_moyen_paiement.dropna() #on supprime Nan
     df_moyen_paiement=drop_existing_name(before_dico_moyen_paiement_db, df_moyen_paiement) #supprime les noms deja existants en bdd
@@ -95,16 +95,16 @@ for filepath in filepaths :
 
 
 
-    #dictionnaires des vendeurs / types activites apres l'insertion
+    #dictionnaires des structures / transactions / moyens de paiement apres l'insertion
     after_dico_type_structure_db=database_to_dict("type_structure",conn)
     after_dico_type_transaction_db=database_to_dict("type_transaction",conn)
     after_dico_moyen_paiement_db=database_to_dict("moyen_paiement",conn)
 
 
-    #transforme les noms de vendeur et type_activite en leur id associe
-    df_commande= df_commande.replace(after_dico_type_structure_db) #on transforme noms de vendeur en leur id
-    df_commande= df_commande.replace(after_dico_type_transaction_db) #on transforme les noms de type_activite en leur id
-    df_commande= df_commande.replace(after_dico_moyen_paiement_db) #on transforme les noms de type_activite en leur id
+    #transforme les noms de structures / transactions / moyens de paiement en leur id associe
+    df_commande= df_commande.replace(after_dico_type_structure_db) #on transforme noms de structures en leur id
+    df_commande= df_commande.replace(after_dico_type_transaction_db) #on transforme les noms de transactions en leur id
+    df_commande= df_commande.replace(after_dico_moyen_paiement_db) #on transforme les noms de moyen de paiement en leur id
 
     #on change les nom de col pour correspondre à la table de la BDD
     df_commande = df_commande.rename(columns={'type_structure_nom': 'type_structure_id', 'type_transaction_nom': 'type_transaction_id','moyen_paiement_nom':'moyen_paiement_id'})
@@ -113,7 +113,7 @@ for filepath in filepaths :
     df_commande['commande_date_achat']=df_commande['commande_date_achat'].transform(lambda x: excel_to_sql_date(x)) #on change "/" en "-" et on inverse jours et ans
 
 
-    df_commande["client_id"]=1
+    df_commande["client_id"]=1 #il faut un champs non nul, on en place un arbitraire
 
     # insert le tableau activite dans la bdd
     df_to_database(df_commande,"commande",conn)
