78f0ca86b74a16f18467ae55697a8c8a4e202288
RomainGuirous <romaing_59@hotmail.fr>
2023-12-18 15:43:03 +0100

    reception read_commande

--- read_activite.py
+++ read_activite.py
@@ -30,7 +30,7 @@ def database_to_dict(table_name, connection) :
         dict_res[i[table_name+'_nom']]=i[table_name+'_id']
     return dict_res
 
-#a partir d'un filepath, renvoie le numero de mois et l'annee correspondante
+
 def str_to_month_year(s) :
     l_mois={
     "janvier" : "01","fevrier" : "02", "février" : "02", "mars" : "03","avril" : "04","mai" : "05","juin" : "06",
@@ -50,6 +50,35 @@ def drop_existing_name(dico, df) :
             res=res.drop(index=i)
     return res
 
+# compare deux chaines de charactere, si ils sont egaux ou tous les deux nuls renvoie True, sinon renvoie False
+def equal_or_both_null(s1, s2) :
+    s1_2=str(s1)
+    s2_2=str(s2)
+    if pd.isnull(s1_2) & pd.isnull(s2_2) :
+        return True
+    if pd.isnull(s1_2) | pd.isnull(s2_2) :
+        return False
+    if s1_2.lower()==s2_2.lower() :
+        return True
+    return False
+
+def add_type_act(df_to_add, connection) :
+    df_res=df_to_add
+    df_from_db = pd.read_sql_query('SELECT type_activite_nom,activite_nom FROM type_activite', connection)
+    df_res=df_res[["type_activite_nom","activite_nom"]]
+    df_res = df_res.astype({"type_activite_nom" : str, "activite_nom" : str})
+    add_type_act=True #un client est par defaut inconnu et doit etre ajoute a la bdd
+    for i in df_res.index :
+        for j in df_from_db.index :
+            # si le nom et prenom en fiche = le nom et prenom en bdd :
+            if equal_or_both_null(df_res.loc[i,"type_activite_nom"], df_from_db.loc[j,"type_activite_nom"]) & equal_or_both_null(df_res.loc[i,"activite_nom"], df_from_db.loc[j,"activite_nom"]):
+                add_type_act=False # si le client est deja connu, on ne l'ajoute pas
+        if add_type_act :
+            type_act_to_add=df_res.loc[[i]] #on recupere la ligne de la fiche a rajouter
+            type_act_to_add.to_sql("type_activite", con=connection, index=False, if_exists='append') # on ajoute le client a la bdd
+            df_from_db = pd.concat([df_from_db, type_act_to_add], ignore_index=True) # ajoute le nouveau client au df local
+        add_type_act=True # on reinitialise la variable pour le prochain client
+
 
 
 #Main
@@ -64,41 +93,30 @@ for filepath in filepaths :
     df=pd.read_csv(filepath)
     df_activite=select_activite(df)
     
-    #dictionnaires des vendeurs / types activites avant l'insertion
-    before_dico_type_activite_db=database_to_dict("type_activite",conn)
-    before_dico_vendeur_db=database_to_dict("vendeur",conn)
-
-
-    # recupere la liste des activites et envoie les nouvelles activites en bdd
-    df_type_activite=df_activite['type_activite_nom'].drop_duplicates() #on supprime doublons -> Nan
-    df_type_activite=df_type_activite.dropna() #on supprime Nan
-    df_type_activite=drop_existing_name(before_dico_type_activite_db, df_type_activite) #supprime les noms deja existants en bdd
-    df_to_database(df_type_activite,"type_activite",conn)
-
-    # recupere la liste des vendeurs et envoie les nouveaux vendeurs en bdd
+    # table vendeur
+    before_dico_vendeur_db=database_to_dict("vendeur",conn)#dictionnaires des vendeurs avant l'insertion
     df_vendeur=df_activite['vendeur_nom'].drop_duplicates() #on supprime doublons -> Nan
     df_vendeur=df_vendeur.dropna() #on supprime Nan
     df_vendeur=drop_existing_name(before_dico_vendeur_db, df_vendeur) #supprime les noms deja existants en bdd
     df_to_database(df_vendeur,"vendeur",conn)
-
-
-
-    #dictionnaires des vendeurs / types activites apres l'insertion
-    after_dico_vendeur_db=database_to_dict("vendeur",conn)
-    after_dico_type_activite_db=database_to_dict("type_activite",conn)
-
-
-    #transforme les noms de vendeur et type_activite en leur id associe
+    after_dico_vendeur_db=database_to_dict("vendeur",conn) #dictionnaires des vendeurs / types activites apres l'insertion
     df_activite= df_activite.replace(after_dico_vendeur_db) #on transforme noms de vendeur en leur id
-    df_activite= df_activite.replace(after_dico_type_activite_db) #on transforme les noms de type_activite en leur id
+    
+    #table type_activite
+    add_type_act(df_activite,conn)
+    after_dico_type_act_db=database_to_dict("type_activite",conn) #dictionnaires des vendeurs / types activites apres l'insertion
+    df_activite= df_activite.replace(after_dico_type_act_db)
+
+
 
     #on change les nom de col pour correspondre à la table de la BDD
-    df_activite = df_activite.rename(columns={'vendeur_nom': 'vendeur_id', 'activite_nom': 'activite_nom','type_activite_nom':'type_activite_id'}) 
+    df_activite = df_activite.rename(columns={'vendeur_nom': 'vendeur_id','type_activite_nom':'type_activite_id'})
+    df_activite = df_activite.drop('activite_nom',axis=1)
 
     #ajoute la colonne mois correspondante au mois trouve dans le nom de fichier
     filename=os.path.basename(filepath)
     mois, annee = str_to_month_year(filename)
     df_activite["activite_mois"]=annee+"-"+mois+"-01" #YYYY/MM/dd
 
-    #insert le tableau activite dans la bdd
+    # insert le tableau activite dans la bdd
     df_to_database(df_activite,"activite",conn)
