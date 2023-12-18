9830bf350470137ba4561daf761ca3cd07128ef5
PierreAnatole <pierranatole@free.fr>
2023-12-18 14:00:38 +0100

    gestion des doublons de commande

--- read_commande.py
+++ read_commande.py
@@ -5,9 +5,6 @@ from sqlalchemy import create_engine
 import re
 pd.set_option('display.max_rows', 500)
 
-
-filepath="./donnees/fiches_mensuelles/Janvier-2023.csv"
-
 # commande
 def select_commmande(df) :
     df_commande = df.iloc[:49,:21] # selectionne uniquement les lignes jusque la ligne 50 du csv et 21 premieres colonnes
@@ -66,7 +63,7 @@ def equal_or_both_null(s1, s2) :
         return True
     if pd.isnull(s1_2) | pd.isnull(s2_2) :
         return False
-    if s1_2.lower()==s2_2.lower() :
+    if s1_2.lower() == s2_2.lower() :
         return True
     return False
 
@@ -100,9 +97,37 @@ def get_clients_id(df_to_get, df_from_db) :
     pd.options.mode.chained_assignment = "warn"
     return df_res
 
-def drop_if_command_exists(df_to_drop, df_from_db) :
-    df_res = pd.concat([df_from_db, df_to_drop], ignore_index=True)
-    df_res = df_res.drop_duplicates(subset=["commande_date_achat", "client_id", "moyen_paiement_id", "type_transaction_id", "type_structure_id"])
+
+def add_new_commands(df_to_add, connection) :
+    df_res = df_to_add # liste des commandes a rajouter en cours
+    df_from_db = pd.read_sql_query('SELECT commande_date_achat, client_id, moyen_paiement_id, type_transaction_id, type_structure_id FROM commande', connection)
+    add_command=True # une commande est ajoutee par defaut
+
+    # compare chacun des champs des deux dataframe entre eux (commande_date_achat, client_id, moyen_paiement_id, type_transaction_id et type_structure_id) en renvoie True
+    # si les 5 champs correspondent entre eux
+    def same_line(i, j, dfcom, dfdb) :
+        c = str(dfcom.loc[i,"commande_date_achat"])
+        c_db = str(dfdb.loc[j,"commande_date_achat"])
+        cli = float(dfcom.loc[i,"client_id"])
+        cli_db = float(dfdb.loc[j,"client_id"])
+        mp = str(dfcom.loc[i,"moyen_paiement_id"])
+        mp_db = str(dfdb.loc[j,"moyen_paiement_id"])
+        tt = str(dfcom.loc[i,"type_transaction_id"])
+        tt_db = str(dfdb.loc[j,"type_transaction_id"])
+        ts = str(dfcom.loc[i,"type_structure_id"])
+        ts_db = str(dfdb.loc[j,"type_structure_id"])
+        if ( (c==c_db) & (cli==cli_db) & (mp==mp_db) & (tt==tt_db) & (ts==ts_db) ) :
+            return True
+        return False
+    for i in df_res.index :
+        for j in df_from_db.index :
+            if same_line(i, j, df_res, df_from_db) :
+                add_command=False # si la commande existe deja, on ne l'ajoute pas
+        if add_command :
+            command_to_add=df_res.loc[[i]] #on recupere la ligne de la commande a rajouter
+            command_to_add.to_sql("commande", con=connection, index=False, if_exists='append') # on ajoute la commande a la bdd
+            df_from_db = pd.concat([df_from_db, command_to_add], ignore_index=True) # ajoute la nouvelle commande au df local
+        add_command=True # on reinitialise la variable pour la prochaine commande
     return df_res
 
 #Main
@@ -114,19 +139,20 @@ for i in range(len(filepaths)) :
     filepaths[i]="./donnees/fiches_mensuelles/"+filepaths[i] #on récupère liste des filepath de chaque fiche mensuelle
 
 for filepath in filepaths :
-    df=pd.read_csv(filepath)
     print(filepath)
+    df=pd.read_csv(filepath)
+    df_activite= pd.read_sql_query('SELECT * FROM activite', conn)
     df_commande=select_commmande(df) #on récupère un dataframe par mois avec les colonnes et les lignes qui nous intéressent
     
     
     # table type_structure
-    before_dico_type_structure_db=database_to_dict("type_structure",conn) # recupere la liste des structures du fichier en cours et envoie les nouvelles structures en bdd
-    df_type_structure=df_commande['type_structure_nom'].drop_duplicates() # suppression des doublons
+    before_dico_type_structure_db=database_to_dict("type_structure",conn) # recupere la liste des structures existantes en bdd
+    df_type_structure=df_commande['type_structure_nom'].drop_duplicates() # suppression des doublons du fichier en cours
     df_type_structure=df_type_structure.dropna() #suppression des Nan
     df_type_structure=drop_existing_name(before_dico_type_structure_db, df_type_structure) # suppression des noms deja existants en bdd
     df_to_database(df_type_structure,"type_structure",conn) # insertion des nouveaux noms de structure en bdd
     after_dico_type_structure_db=database_to_dict("type_structure",conn) # dictionnaires des structures apres l'insertion
-    df_commande= df_commande.replace(after_dico_type_structure_db) # remplacement des noms de structure par leur id
+    df_commande = df_commande.replace(after_dico_type_structure_db) # remplacement des noms de structure locals par leur id
 
 
     # meme chose avec la table type_transaction
@@ -136,7 +162,7 @@ for filepath in filepaths :
     df_type_transaction=drop_existing_name(before_dico_type_transaction_db, df_type_transaction)
     df_to_database(df_type_transaction,"type_transaction",conn)
     after_dico_type_transaction_db=database_to_dict("type_transaction",conn)
-    df_commande= df_commande.replace(after_dico_type_transaction_db)
+    df_commande = df_commande.replace(after_dico_type_transaction_db)
 
 
     # meme chose avec la table moyen_paiement
@@ -146,14 +172,14 @@ for filepath in filepaths :
     df_moyen_paiement=drop_existing_name(before_dico_moyen_paiement_db, df_moyen_paiement)
     df_to_database(df_moyen_paiement,"moyen_paiement",conn)
     after_dico_moyen_paiement_db=database_to_dict("moyen_paiement",conn)
-    df_commande= df_commande.replace(after_dico_moyen_paiement_db)
+    df_commande = df_commande.replace(after_dico_moyen_paiement_db)
 
 
     # table clients
     add_new_clients(df_commande, conn) # ajoute les client du fichier qui n'existent pas dans la bdd
     df_from_db = pd.read_sql_query('SELECT client_id, client_nom, client_prenom FROM client', conn) # recupere la liste des clients de la bdd
     df_commande=get_clients_id(df_commande, df_from_db) # compare chaque client du fichier avec ceux de la bdd et leur associe leur id en rajoutant client_id au dataframe
-    df_commande=df_commande.drop(["client_nom","client_prenom"],axis=1) # retire les colonnes client_nom et client_prenom qui n'apparaissent pas en bdd
+    df_commande=df_commande.drop(["client_nom", "client_prenom"], axis=1) # retire les colonnes client_nom et client_prenom qui n'apparaissent pas en bdd
 
 
     # change les nom de col pour correspondre à la table de la BDD
@@ -162,10 +188,4 @@ for filepath in filepaths :
     # change le format de dates pour correspondre aux normes sql : de JJ/MM/AAAA -> AAAA-MM-JJ
     df_commande['commande_date_achat']=df_commande['commande_date_achat'].transform(lambda x: excel_to_sql_date(x)) #on change "/" en "-" et on inverse jours et ans
 
-    #TODO drop les doublons de commande
-    # df_from_db = pd.read_sql_query('SELECT commande_id, commande_date_achat, client_id, moyen_paiement_id, type_transaction_id, type_structure_id FROM commande', conn)
-    # print(drop_if_command_exists(df_commande, df_from_db))
-
-
-    # insert les commandes dans la bdd
-    df_to_database(df_commande, "commande", conn)
+    add_new_commands(df_commande, conn) # ajoute les commandes une par une dans la bdd
