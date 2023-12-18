c8867ec87dd42e485c2577ac5cb6aba2f556f2af
RomainGuirous <romaing_59@hotmail.fr>
2023-12-18 12:07:25 +0100

    new looping

--- create_db.sql
+++ create_db.sql
@@ -3,6 +3,89 @@ CREATE DATABASE eviesens;
 
 USE eviesens;
 
+-- CREATE TABLE client(
+--    client_id INT AUTO_INCREMENT,
+--    client_nom VARCHAR(155),
+--    client_prenom VARCHAR(155),
+--    client_mail VARCHAR(155),
+--    client_telephone VARCHAR(155),
+--    PRIMARY KEY(client_id)
+-- );
+
+-- CREATE TABLE vendeur(
+--    vendeur_id INT AUTO_INCREMENT,
+--    vendeur_nom VARCHAR(155) NOT NULL,
+--    PRIMARY KEY(vendeur_id)
+-- );
+
+-- CREATE TABLE type_activite(
+--    type_activite_id INT AUTO_INCREMENT,
+--    type_activite_nom VARCHAR(155) NOT NULL,
+--    PRIMARY KEY(type_activite_id)
+-- );
+
+-- CREATE TABLE activite(
+--    activite_id INT AUTO_INCREMENT,
+--    activite_prix DECIMAL(15,2),
+--    activite_nom VARCHAR(155) NOT NULL,
+--    activite_mois DATE,
+--    type_activite_id INT NOT NULL,
+--    vendeur_id INT NOT NULL,
+--    PRIMARY KEY(activite_id),
+--    FOREIGN KEY(type_activite_id) REFERENCES type_activite(type_activite_id),
+--    FOREIGN KEY(vendeur_id) REFERENCES vendeur(vendeur_id)
+-- );
+
+-- CREATE TABLE type_structure(
+--    type_structure_id INT AUTO_INCREMENT,
+--    type_structure_nom VARCHAR(155) NOT NULL,
+--    PRIMARY KEY(type_structure_id)
+-- );
+
+-- CREATE TABLE type_transaction(
+--    type_transaction_id INT AUTO_INCREMENT,
+--    type_transaction_nom VARCHAR(155) NOT NULL,
+--    PRIMARY KEY(type_transaction_id)
+-- );
+
+-- CREATE TABLE moyen_paiement(
+--    moyen_paiement_id INT AUTO_INCREMENT,
+--    moyen_paiement_nom VARCHAR(155) NOT NULL,
+--    PRIMARY KEY(moyen_paiement_id)
+-- );
+
+-- CREATE TABLE commande(
+--    commande_id INT AUTO_INCREMENT,
+--    commande_date_achat DATE NOT NULL,
+--    client_id INT NOT NULL,
+--    moyen_paiement_id INT NOT NULL,
+--    type_transaction_id INT NOT NULL,
+--    type_structure_id INT NOT NULL,
+--    PRIMARY KEY(commande_id),
+--    FOREIGN KEY(client_id) REFERENCES client(client_id),
+--    FOREIGN KEY(moyen_paiement_id) REFERENCES moyen_paiement(moyen_paiement_id),
+--    FOREIGN KEY(type_transaction_id) REFERENCES type_transaction(type_transaction_id),
+--    FOREIGN KEY(type_structure_id) REFERENCES type_structure(type_structure_id)
+-- );
+
+-- CREATE TABLE commande_activite(
+--    activite_id INT,
+--    commande_id INT,
+--    commande_date_soin DATE,
+--    commande_quantité DECIMAL(15,2) NOT NULL,
+--    commande_deplacement DECIMAL(15,2) NOT NULL,
+--    commande_reduction DECIMAL(15,2) NOT NULL,
+--    commande_date_encaissement DATE,
+--    commande_date_perception DATE,
+--    commande_date_remboursement DATE,
+--    PRIMARY KEY(activite_id, commande_id),
+--    FOREIGN KEY(activite_id) REFERENCES activite(activite_id),
+--    FOREIGN KEY(commande_id) REFERENCES commande(commande_id)
+-- );
+
+#####################################################
+
+
 CREATE TABLE client(
    client_id INT AUTO_INCREMENT,
    client_nom VARCHAR(155),
@@ -21,13 +104,13 @@ CREATE TABLE vendeur(
 CREATE TABLE type_activite(
    type_activite_id INT AUTO_INCREMENT,
    type_activite_nom VARCHAR(155) NOT NULL,
+   activite_nom VARCHAR(155) NOT NULL,
    PRIMARY KEY(type_activite_id)
 );
 
 CREATE TABLE activite(
    activite_id INT AUTO_INCREMENT,
    activite_prix DECIMAL(15,2),
-   activite_nom VARCHAR(155) NOT NULL,
    activite_mois DATE,
    type_activite_id INT NOT NULL,
    vendeur_id INT NOT NULL,
@@ -82,3 +165,6 @@ CREATE TABLE commande_activite(
    FOREIGN KEY(activite_id) REFERENCES activite(activite_id),
    FOREIGN KEY(commande_id) REFERENCES commande(commande_id)
 );
+
+
+
--- read_commande_activite.py
+++ read_commande_activite.py
@@ -3,7 +3,7 @@ import numpy as np
 import os
 from sqlalchemy import create_engine
 import re
-pd.set_option('display.max_rows', 500)
+pd.set_option('display.max_rows', 401)
 
 
 filepath="./donnees/fiches_mensuelles/Janvier-2023.csv"
@@ -19,8 +19,8 @@ filepath="./donnees/fiches_mensuelles/Janvier-2023.csv"
     #commande_perception
     #commande_date_remboursement
 df=pd.read_csv(filepath)
-# df=df.iloc[:49,:21]
-# print(df)
+df=df.iloc[:49,:21]
+print(df)
 def select_commmande_activite(df) :
     df_commande = df.iloc[:49,:21] # selectionne uniquement les lignes jusque la ligne 50 du csv et 21 premieres colonnes
     df_commande = df_commande[df_commande['Structure'].notna()]
@@ -62,17 +62,52 @@ def date_jour_fixe(date):
     date=re.sub(r"(\d{4}-\d\d)-\d\d",r"\1-01",date)
     return date
 
+# compare deux chaines de charactere, si ils sont egaux ou tous les deux nuls renvoie True, sinon renvoie False
+def equal_or_both_null(s1, s2) :
+    s1_2=str(s1)
+    s2_2=str(s2)
+    if pd.isnull(s1_2) & pd.isnull(s2_2) :
+        return True
+    if pd.isnull(s1_2) | pd.isnull(s2_2) :
+        return False
+    if s1_2.lower()==s2_2.lower() :
+        return True
+    return False
+
 # si dans une ligne de la table activite le couple (activite_nom, activite_mois) est egal au couple (activite_nom,commande_date_achat) d'une ligne la table commande_activite
 # il rajoute à la ligne de commande_activite son activite_id
-def comp_activite_commande_activite(df_acti, df_com_acti) :
+def add_id_df(df_acti, df_com_acti) :
     x=df_com_acti
-    x['activite_id']=np.nan
-    x['commande_date_achat']=x['commande_date_achat'].tranform(lambda x: date_jour_fixe(x))
+    x['activite_id']=np.nan #pour créer colonne, on la remplit de vides
+    x['commande_date_achat_test']=x['commande_date_achat'].transform(lambda x: date_jour_fixe(x)) #on crée une nouvelle colonne avec le jour fixe (01) qui va permettre de comparer (comme ça la vraie date reste intact)
     for i in df_acti.index :
         for j in x.index :
-            if (df_acti["activite_nom"][i]==x["activite_nom"][j]) & (df_acti["activite_mois"][i]==x["commande_date_achat"][j]):
-                x.loc[j,['activite_id']]=df_acti.loc[i,['activite_id']]
+            if equal_or_both_null(df_acti["activite_nom"][i],x["activite_nom"][j]) & equal_or_both_null(df_acti["activite_mois"][i],x["commande_date_achat_test"][j]): #on s'assure de ne pas avoir de vide et de tout mettre en minuscule
+                x.loc[j,['activite_id']]=df_acti.loc[i,['activite_id']]   
+    x=x.drop(['commande_date_achat_test'],axis=1) #on supprime la colonne de commparaison qui ne sert plus
     return x
+
+def add_id_df2(df_com, df_com_acti) :
+    x=df_com_acti
+    x['commande_id']=np.nan #pour créer colonne, on la remplit de vides
+    # x['commande_date_achat_test']=x['commande_date_achat'].transform(lambda x: date_jour_fixe(x)) #on crée une nouvelle colonne avec le jour fixe (01) qui va permettre de comparer (comme ça la vraie date reste intact)
+    for i in df_com.index :
+        for j in x.index :
+            if equal_or_both_null(df_com["activite_nom"][i],x["activite_nom"][j]) & equal_or_both_null(df_com["commande_date_achat"][i],x["commande_date_achat"][j]): #on s'assure de ne pas avoir de vide et de tout mettre en minuscule
+                x.loc[j,['activite_id']]=df_com.loc[i,['activite_id']]   
+    x=x.drop(['commande_date_achat_test'],axis=1) #on supprime la colonne de commparaison qui ne sert plus
+    return x
+
+def get_clients_id(df_to_get, df_from_db) :
+    pd.options.mode.chained_assignment = None
+    df_res=df_to_get
+    df_res["client_id"]=np.nan # initialise tous les id a null
+    for i in df_res.index :
+        for j in df_from_db.index :
+            if equal_or_both_null(df_res.loc[i,"client_nom"], df_from_db.loc[j,"client_nom"]) & equal_or_both_null(df_res.loc[i,"client_prenom"], df_from_db.loc[j,"client_prenom"]):
+                df_res.loc[i, "client_id"]=df_from_db.loc[j, "client_id"] # quand un couple nom/prenom est trouve dans la bdd, son id lui est associe
+    pd.options.mode.chained_assignment = "warn"
+    return df_res
 
 #Main
 conn=create_engine('mysql+mysqlconnector://root:root@localhost:3306/eviesens')
@@ -83,26 +118,24 @@ for i in range(len(filepaths)) :
     filepaths[i]="./donnees/fiches_mensuelles/"+filepaths[i] #on récupère liste des filepath de chaque fiche mensuelle
 
 for filepath in filepaths :
+    print(filepath)
     df=pd.read_csv(filepath)
     df_commande_activite=select_commmande_activite(df)
 
+    #on met les dates au format yyyy-mm-dd
+    df_commande_activite['commande_date_achat'] =df_commande_activite['commande_date_achat'].transform(lambda x: excel_to_sql_date(x))
+    df_commande_activite['commande_date_soin'] =df_commande_activite['commande_date_soin'].transform(lambda x: excel_to_sql_date(x))
+    df_commande_activite['commande_perception'] =df_commande_activite['commande_perception'].transform(lambda x: excel_to_sql_date(x))
+    df_commande_activite['commande_date_remboursement'] =df_commande_activite['commande_date_remboursement'].transform(lambda x: excel_to_sql_date(x))
+    df_commande_activite['commande_date_encaissement'] =df_commande_activite['commande_date_encaissement'].transform(lambda x: excel_to_sql_date(x))
 
-df_commande_activite=df_commande_activite[['commande_date_achat','commande_date_soin','commande_perception', 'commande_date_remboursement', 'commande_date_encaissement']]
-df_commande_activite['commande_date_achat'] =df_commande_activite['commande_date_achat'].transform(lambda x: excel_to_sql_date(x))
-df_commande_activite['commande_date_soin'] =df_commande_activite['commande_date_soin'].transform(lambda x: excel_to_sql_date(x))
-df_commande_activite['commande_perception'] =df_commande_activite['commande_perception'].transform(lambda x: excel_to_sql_date(x))
-df_commande_activite['commande_date_remboursement'] =df_commande_activite['commande_date_remboursement'].transform(lambda x: excel_to_sql_date(x))
-df_commande_activite['commande_date_encaissement'] =df_commande_activite['commande_date_encaissement'].transform(lambda x: excel_to_sql_date(x))
+    df_activite= pd.read_sql_query('SELECT * FROM activite', conn) #on appelle df_activite pour l'injecter dans la fonction add_id_df()
 
-# df_activite=pd.read_sql_query('SELECT * FROM activite', conn)
-# df_inner_join=df_commande_activite.join(df_activite.set_index('activite_nom'), on='activite_nom',how='inner')
+    df_commande_activite=add_id_df(df_activite,df_commande_activite) #on ajoute la colonne activite_id à df_commande_activite
+    df_commande_activite=df_commande_activite.drop(['activite_nom'],axis=1) #on supprime activite_nom qui n'est pas dans la table commande_activite
+    
+    df_client=pd.read_sql_query('SELECT * FROM client', conn) #on appelle df_client pour l'injecter dans la fonction add_id_df()
 
-df_activite= pd.read_sql_query('SELECT * FROM activite', conn)
-df_activite=df_activite[['activite_id','activite_nom','activite_mois']] #on prend nom et mois pour pouvoir comparer, id est ce que nous voulons
+    # print(df_activite)
 
-
-
-
-# print(df_activite)
-
-# print(df_commande_activite)
+    # print(df_commande_activite)
